42b8617c3ef68d9cf42206a75b7f8a6b47f45a7f
WT Consulting <wt.consulting@outlook.com>
2022-11-18 12:23:07 -0800

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by WilliamD on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
